4a62ca9a839be707b41d97b86e63d97fc6f2a7cb
Johan Hay <johanhay98@gmail.com>
2025-11-08 15:41:08 +0200

    Apply .gitignore and remove cached ignored files

---
+++ src/main/java/za/co/datedeals/api/DatedealsApplication.java
@@ -0,0 +1,13 @@
+package za.co.datedeals.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatedealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatedealsApplication.class, args);
+	}
+
+}
