2fd99876b1abc9f701fc360aa7678fa7228ce7c4
Johan Hay <johanhay98@gmail.com>
2025-11-26 18:09:19 +0200

    log test

--- backend/src/main/java/za/co/datedeals/api/controllers/LogController.java
+++ backend/src/main/java/za/co/datedeals/api/controllers/LogController.java
@@ -3,8 +3,11 @@ package za.co.datedeals.api.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,14 +22,15 @@ public class LogController {
     private MailService mailService;
     
     @PostMapping(path = "/log-request")
-    public String logRequest(@RequestBody String entity) {
-        logger.info(entity);
+    public String logRequest(@RequestBody String entity, @RequestHeader Map<String, String> headers) {
+        logger.info("Body: {}", entity);
+        logger.info("Headers: {}", headers);
 
-        mailService.sendEmail(
-            "johanhay98@gmail.com",
-            "New Request Logged",
-            "<h1>Request Received</h1><p>" + entity + "</p>"
-        );
+        // mailService.sendEmail(
+        //     "johanhay98@gmail.com",
+        //     "New Request Logged",
+        //     "<h1>Request Received</h1><p>" + entity + "</p>"
+        // );
         return entity;
     }
 }
